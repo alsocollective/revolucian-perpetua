7f218e7780860309c13d291e8a88ee09a0232f77
Bohdan Anderson <bohdan@alsocollective.ccom>
2014-07-21 17:44:42 -0400

    added some things...

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -21,31 +21,103 @@ controllers.selectColours = function($scope, socket) {
 
 }
 
-controllers.tapping = function($scope, socket, UserSet, $location) {
-	console.log("strted gyro");
+controllers.tapping = function($scope, socket, UserSet, $location, $timeout) {
+	console.log("started tapping controller");
 
-	scope = $scope;
-	var ax = 0,
-		ay = 0,
-		az = 0;
+	$scope.socket = socket;
+
+	var averageList = [0, 0, 0, 0, 0],
+		avLength = averageList.length,
+		average = 0,
+		difference = 500,
+		time = (new Date()).getTime(),
+		arrayOfDiff = [],
+		diffLength = avLength,
+		diffAvg = 0,
+		diffoffSet = 0;
+
+	$scope.active = true;
+
+
 	if (window.DeviceMotionEvent) {
-		window.ondevicemotion = function(event) {
-			if (ax < event.accelerationIncludingGravity.x - 1 || ax > event.accelerationIncludingGravity.x + 1 || ay < event.accelerationIncludingGravity.y - 1 || ay > event.accelerationIncludingGravity.y + 1) {
-				ax = event.accelerationIncludingGravity.x;
-				ay = event.accelerationIncludingGravity.y;
-				console.log(Math.floor(ax * 100) + " " + Math.floor(ay * 100));
-				socket.emit("msg", (ax.toString() + " " + ay.toString()));
-				
-				scope.message = (Math.floor(ax * 100) + " " + Math.floor(ay * 100));
+		window.ondevicemotion = deviceTapDetect;
+	}
+
+
+	function deviceTapDetect(event, socket) {
+		if (event.accelerationIncludingGravity.z) {
+			var current = Math.floor(event.accelerationIncludingGravity.z * 1000);
+			if ((average - current > 500 && average - current < 3000) && current < averageList[avLength]) {
+				var nTime = (new Date()).getTime();
+				if (nTime - time > 500) {
+					var scope = angular.element(main_container).scope();
+					scope.$apply(function() {
+						scope.active = false;
+						// console.log(scope.socket.on)
+						scope.socket.emit('tapped', {
+							"device": 233
+						})
+					})
+					$timeout(function() {
+						scope.$apply(function() {
+							var scope = angular.element(main_container).scope();
+						})
+						scope.active = true;
+					}, 50);
+					time = nTime;
+				}
 			}
+			//add new Value to array and remove oldest
+			averageList.shift();
+			averageList[avLength] = current;
+			findAverage();
 		}
 	}
 
-	// $scope.$on("$destroy", function(event, data) {
-	// 	console.log("EXITING this state");
-	// 	console.log(event);
-	// 	console.log(data);
-	// })
+	$scope.triggerTapped = function() {
+		var nTime = (new Date()).getTime();
+		if (nTime - time > 300) {
+			$scope.active = false;
+			socket.emit('tapped', {
+				"device": 233
+			})
+			$timeout(function() {
+				$scope.active = true;
+			}, 50);
+			time = nTime;
+		}
+	}
+
+	function findAverage() {
+		var a = 0,
+			preAv = average,
+			diffadding = 0;
+
+		average = 0,
+		difference = 0;
+		for (a = 0; a < avLength; ++a) {
+			average += averageList[a];
+			difference += preAv - averageList[a]
+			diffadding += arrayOfDiff[a];
+		}
+		difference = Math.floor(difference / avLength);
+		average = Math.floor(average / avLength);
+
+		arrayOfDiff.shift();
+		arrayOfDiff[diffLength] = difference;
+		diffAvg = Math.floor(diffadding / diffLength)
+		diffoffSet = difference - diffAvg
+
+
+		if (!average) {
+			average = 0;
+		}
+	}
+
+	$scope.$on("$destroy", function() {
+		window.ondevicemotion = null;
+	});
+
 }
 
 controllers.login = function($scope, socket, UserSet, $location) {
@@ -69,59 +141,61 @@ controllers.lobby = function($scope, socket, UserSet, $location) {
 
 	var today = new Date();
 	var dd = today.getDate();
-	var mm = today.getMonth()+1; //January is 0!
+	var mm = today.getMonth() + 1; //January is 0!
 	var yyyy = today.getFullYear();
 	var startTime = "16:08:00";
 
-	var target_date = new Date(mm+" "+dd+", "+yyyy+", "+startTime).getTime();
+	var target_date = new Date(mm + " " + dd + ", " + yyyy + ", " + startTime).getTime();
 	var hours, minutes, seconds;
 	var timer = document.getElementById("timer");
 
 	console.log("You've reached the LOBBY!");
 
 	$('.autoplay').slick({
-        dots: false,
-        arrows: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000
-    });
+		dots: false,
+		arrows: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000
+	});
 
-	var timerId = setInterval(function () {
-	 
-	    var current_date = new Date().getTime();
-	    var seconds_left = (target_date - current_date) / 1000;
-	 	     
-	    hours = parseInt(seconds_left / 3600);
-	    seconds_left = seconds_left % 3600;
-	     
-	    minutes = parseInt(seconds_left / 60);
-	    seconds = parseInt(seconds_left % 60);
+	var timerId = setInterval(function() {
 
-	    timer.innerHTML = ("0" + hours).slice(-2)+" : "+("0" + minutes).slice(-2)+" : "+("0" + seconds).slice(-2);
+		var current_date = new Date().getTime();
+		var seconds_left = (target_date - current_date) / 1000;
 
-	    if(hours <= 0 && minutes <= 0 && seconds <= 0) {
+		hours = parseInt(seconds_left / 3600);
+		seconds_left = seconds_left % 3600;
 
-	    	timer.innerHTML = "00 : 00 : 00";
-	   		console.log("PARTY TIME!");
+		minutes = parseInt(seconds_left / 60);
+		seconds = parseInt(seconds_left % 60);
 
-	   		showStart();
+		timer.innerHTML = ("0" + hours).slice(-2) + " : " + ("0" + minutes).slice(-2) + " : " + ("0" + seconds).slice(-2);
 
-	    }	 
+		if (hours <= 0 && minutes <= 0 && seconds <= 0) {
+
+			timer.innerHTML = "00 : 00 : 00";
+			console.log("PARTY TIME!");
+
+			showStart();
+
+		}
 	}, 1000);
 
 	var showStart = function() {
 
 		$location.path('/tapping');
 		/*window.location.href = "/#/tapping";*/ //This is wrong!
-  		//$location.replace();
-  		clearInterval(timerId);
+		//$location.replace();
+		clearInterval(timerId);
 	}
 
-	socket.on('CP', function(data) {$location.path(data)})
+	socket.on('CP', function(data) {
+		$location.path(data)
+	})
 }
 
 controllers.admin = function($scope, socket, UserSet, $location) {
