a0e672ada16172d3fa333ff9e4c7c668d2767355
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2014-10-14 18:13:05 -0400

    ready for server?

--- public/app/factory.js
+++ public/app/factory.js
@@ -1,29 +1,7 @@
 var factories = {};
 
 factories.socket = function($rootScope, $location) {
-<<<<<<< HEAD
-    var socket = io.connect("192.168.0.103:3000");
-    return {
-        on: function(eventName, callback) {
-            socket.on(eventName, function(msg) {
-                $rootScope.$apply(function() {
-                    callback.apply(socket, [msg]);
-                });
-            });
-        },
-        emit: function(eventName, data, callback) {
-            socket.emit(eventName, data, function() {
-                var args = arguments;
-                $rootScope.$apply(function() {
-                    if (callback) {
-                        callback.apply(socket, args);
-                    }
-                });
-            })
-        }
-    };
-=======
-	var socket = io.connect("192.168.0.102:3000");
+	var socket = io.connect("192.168.0.158:3000");
 	return {
 		on: function(eventName, callback) {
 			socket.on(eventName, function(msg) {
@@ -43,7 +21,6 @@ factories.socket = function($rootScope, $location) {
 			})
 		}
 	};
->>>>>>> f8be43e5d4fa5e54d45dc454160d59a110d72921
 }
 
 factories.UserSet = function($cookies, socket, socket, $location) {
