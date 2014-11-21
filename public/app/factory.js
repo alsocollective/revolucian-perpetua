46fe0d14c93db3175dce01ba0d8f479b692e3b2b
bohdan <bohdan.anderson@gmail.com>
2014-11-21 04:29:07 -0500

    yessss small things

--- public/app/factory.js
+++ public/app/factory.js
@@ -2,7 +2,7 @@ var factories = {};
 
 factories.Socket = function($rootScope, $location) {
 
-	var socket = io.connect("http://192.168.2.25:3000");
+	var socket = io.connect("http://192.168.1.101:3000");
 
 
 	return {
